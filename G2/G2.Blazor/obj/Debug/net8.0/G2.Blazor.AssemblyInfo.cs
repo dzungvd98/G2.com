a31e333b3cab13c45d88e3cdffafa6e0cebce26c
Hồ Chí Tài <hochitai24@gmail.com>
2024-12-28 00:09:26 +0700

    fix breakpoint for screen full hd

--- G2/G2.Blazor/obj/Debug/net8.0/G2.Blazor.AssemblyInfo.cs
+++ G2/G2.Blazor/obj/Debug/net8.0/G2.Blazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G2.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca05596a58ea5e10d62226796c2dae8ee108e550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bce6e06e05da746f1ba65ed9ff6533a19b0f3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("G2.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G2.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- G2/G2.Blazor/tailwind.config.js
+++ G2/G2.Blazor/tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
     content: ['./**/*.{razor,html}'],
     theme: {
         extend: {
+            screens: {
+                '2xl': '1280px', // Hd 1920x1080
+            },
             colors: {
                 primary: "var(--primary-color)",
                 secondary: "var(--secondary-color)",
                 subtext: "var(--subtext-color)"
             },
-},
+        },
     },
     plugins: [],
 }
