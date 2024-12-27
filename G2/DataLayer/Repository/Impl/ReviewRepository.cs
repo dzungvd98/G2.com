55565247179aaa0091a9a10e1ba59b1e454ec7fe
GiaHuy2505 <phangiahuy25052003@gmail.com>
2024-12-27 17:12:27 +0700

    create review API

---
+++ G2/DataLayer/Repository/Impl/ReviewRepository.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer.Database;
+using DataLayer.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository.Impl
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewRepository(ApplicationDbContext context) { 
+            _context = context;
+        }
+        public async Task<IEnumerable<ReviewDTO>> GetReviewByProductID(int productID)
+        {
+            var review = await _context.Reviews
+            .Where(p => p.ProductId == productID)
+            .Select(p => new ReviewDTO
+            {
+
+            })
+            .ToListAsync();
+            return review;
+        }
+    }
+}
