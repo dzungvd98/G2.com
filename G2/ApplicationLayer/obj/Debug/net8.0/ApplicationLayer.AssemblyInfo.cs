af50679d0e9547597afacbcab51bd0d21b53a25a
dzungvd98 <yungvd98@gmail.com>
2025-01-09 17:14:49 +0700

    Update log

--- G2/ApplicationLayer/obj/Debug/net8.0/ApplicationLayer.AssemblyInfo.cs
+++ G2/ApplicationLayer/obj/Debug/net8.0/ApplicationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca5e04705838ee8f7d93404653e4127b713da795")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08c4d9c8bb6878895285a7c99d2d011f9bd342a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApplicationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
