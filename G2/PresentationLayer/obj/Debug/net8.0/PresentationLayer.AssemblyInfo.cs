08e7eea603400d12ed0db3760ece49b2aa0a1edc
dzungvd98 <yungvd98@gmail.com>
2025-01-11 14:30:42 +0700

    reset

--- G2/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ G2/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af50679d0e9547597afacbcab51bd0d21b53a25a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be0bdddef87610c048d39e11b877e567f7c8e8bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
