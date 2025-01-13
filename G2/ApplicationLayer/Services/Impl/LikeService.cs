d7c0560018697a434477abae66b51e1fa30a4c62
GiaHuy2505 <phangiahuy25052003@gmail.com>
2025-01-13 10:19:11 +0700

    Create Like API

---
+++ G2/ApplicationLayer/Services/Impl/LikeService.cs
@@ -0,0 +1,40 @@
+﻿using DataLayer.DTO;
+using DataLayer.Models;
+using DataLayer.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Services.Impl
+{
+    public class LikeService : ILikeService
+    {
+        private readonly ILikeRepository _likeRepository;
+
+        public LikeService(ILikeRepository likeRepository)
+        {
+            _likeRepository = likeRepository;
+        }
+
+        public async Task<Like> AddLikeAsync(AddLikeDTO addLikeDTO)
+        {
+            var existingLike = await _likeRepository.GetLikeByUserAndReviewAsync(addLikeDTO.UserId, addLikeDTO.ReviewId);
+            if (existingLike != null)
+            {
+                throw new InvalidOperationException("User has already liked this review.");
+            }
+
+            var like = new Like
+            {
+                UserId = addLikeDTO.UserId,
+                ReviewId = addLikeDTO.ReviewId,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            return await _likeRepository.AddLikeAsync(like);
+        }
+    }
+}
---
+++ G2/DataLayer/Repository/Impl/LikeRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer.Database;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository.Impl
+{
+    public class LikeRepository : ILikeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LikeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Like?> GetLikeByUserAndReviewAsync(int userId, int reviewId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(l => l.UserId == userId && l.ReviewId == reviewId);
+        }
+
+        public async Task<Like> AddLikeAsync(Like like)
+        {
+            _context.Likes.Add(like);
+            await _context.SaveChangesAsync();
+            return like;
+        }
+    }
+}
